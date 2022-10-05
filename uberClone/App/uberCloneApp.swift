5ecfa46b33de35b15cdc2878d4032ab379f28867
Mahesh Adhikari <maheshadhikari@live.com>
2022-10-05 01:02:39 -0400

    Initial Commit

---
+++ uberClone/App/uberCloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  uberCloneApp.swift
+//  uberClone
+//
+//  Created by Mahesh Adhikari on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct uberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+            
+        }
+    }
+}
